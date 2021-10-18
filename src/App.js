c9c9e54e42a653f8af1b55f2a7a779e753e6f1f8
muzammil-aslam <muzammilaslam914@gmail.com>
2021-10-18 14:39:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inpValue, setInpvalue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const itemEvent = (event) => {
+    setInpvalue(event.target.value);
+  };
+
+  const handel = () => {
+    var newTodo = [...todo];
+    newTodo.push(inpValue);
+    setTodo(newTodo);
+    setInpvalue("");
+  };
+
+  const deleteFunction = (index) => {
+    var newTodo = [...todo];
+    newTodo.splice(index, 1);
+    setTodo(newTodo);
+  };
+
+  const reefresh = () => {
+    setTodo([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="child">
+        <div className="newChwild">
+          <input
+            value={inpValue}
+            onChange={itemEvent}
+            placeholder="enter your name"
+          />
+          <button onClick={handel}>+</button>
+          <button onClick={reefresh}>All Delete</button>
+
+          {todo.map((itemval, index) => {
+            return (
+              <div key={index}>
+                <span>{itemval}</span>
+                <button
+                  onClick={() => {
+                    deleteFunction(index);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
